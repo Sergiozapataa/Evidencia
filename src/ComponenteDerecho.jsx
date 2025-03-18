308e6dec9454a030b35a227ace67f3bc611885e8
Sergiozapataa <sergiozapataalvarez45@gmail.com>
2025-03-18 18:31:50 -0500

    Nuevos archivos

--- src/ComponenteDerecho.jsx
+++ src/ComponenteDerecho.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ComponenteDerecho() {
   return (
-    <div className='Contenedorverde'>ComponenteDerecho</div>
+    <div className='Contenedorverde'></div>
   )
 }
